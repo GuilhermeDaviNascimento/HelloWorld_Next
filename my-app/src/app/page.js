560823b336df8022db2be651ab7b29df0ccdcd35
GuilhermeDaviNascimento <131400280+GuilhermeDaviNascimento@users.noreply.github.com>
2024-12-11 08:55:11 -0300

    comit

---
+++ my-app/src/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Olá, Nees</h1>
+      {/* testando a lib */}
+      <Button>Click me</Button>
+    </div>
+  );
+}
